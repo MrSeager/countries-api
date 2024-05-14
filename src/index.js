96db6e1a9866ec15d6dffaa3042a3eb2d22c6054
MrSeager <seager1993@gmail.com>
2024-05-14 10:59:32 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Container, Nav, Card, CardTitle} from 'react-bootstrap';
+import autoAnimate from '@formkit/auto-animate';
+import { useEffect, useRef } from 'react';
+
+const MainPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect (() => {
+    fetch('https://raw.githubusercontent.com/MrSeager/countries-api/main/src/data.json')
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error('Error', error));
+  }, []);
+
+  if (!data) {
+    return <p>Loading data...</p>;
+  }
+
+  return (
+    <Container fluid>
+      <Nav>
+
+      </Nav>
+      <Container fluid className='cs-grid'>
+        {data.map((country, index) => (
+          <Card key={index} className='pb-3'>
+            <Card.Img fluid variant='top' src={country.flag} className='cs-img' />
+            <Card.Title className='px-3 mt-3 h6 fw-bold'>{country.name}</Card.Title>
+            <Card.Text className='px-3 m-0 cs-fs'><b>Population:</b> {country.population}</Card.Text>
+            <Card.Text className='px-3 m-0 cs-fs'><b>Region:</b> {country.region}</Card.Text>
+            <Card.Text className='px-3 m-0 cs-fs'><b>Capital:</b> {country.capital}</Card.Text>
+          </Card>
+        ))}
+      </Container>
+    </Container>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>
 );
 
